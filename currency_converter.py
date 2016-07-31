497e464752d39a83af90f466966c0daaeb743165
JeremyChen <jeremychen0127@gmail.com>
2016-07-30 23:22:23 -0400

    Added conversion mode

--- currency_converter.py
+++ currency_converter.py
@@ -18,10 +18,13 @@ arg_parser.add_argument('-s', '--show', help=helps['show'], nargs='+', metavar='
 arg_parser.add_argument('-c', '--convert', help=helps['convert'], nargs=3, metavar=convert_metavar)
 args = arg_parser.parse_args()
 
-# requests the rates on given date
+# variable initializations
 date = 'latest'
 url = 'http://api.fixer.io/'
 url_params = ''
+amount = '1'
+
+# requests the rates on given date
 if (args.date):
   month = args.date[1].zfill(2)
   day = args.date[2].zfill(2)
@@ -29,18 +32,24 @@ if (args.date):
 
 url += date
 
-# changes base currency
-if (args.base):
-  url_params += 'base=' + args.base
+# conversion mode
+if (args.convert):
+  amount = args.convert[0]
+  url_params += 'base=' + args.convert[1]
+  url_params += '&symbols=' + args.convert[2]
 else:
-  url_params += 'base=USD'
-
-# only shows specified currencies
-if (args.show):
-  currencies_to_show = args.show
-  url_params += '&symbols=' + currencies_to_show[0]
-  for cur in currencies_to_show[1:]:
-    url_params += ',' + cur
+  # changes base currency
+  if (args.base):
+    url_params += 'base=' + args.base
+  else:
+    url_params += 'base=USD'
+  
+  # only shows specified currencies
+  if (args.show):
+    currencies_to_show = args.show
+    url_params += '&symbols=' + currencies_to_show[0]
+    for cur in currencies_to_show[1:]:
+      url_params += ',' + cur
 
 # forms the final url for api call
 url += '?' + url_params
@@ -55,9 +64,9 @@ rates = json.loads(rates_json_string)
 print 'Date of Rates: ' + rates['date'] + '\n'
 
 # prints currency base and amount
-print rates['base'] + ': 1'
+print rates['base'] + ': ' + amount
 print '---------------------------'
 
 # prints the rates
 for cur, rate in rates['rates'].iteritems():
-  print cur + ': ' + str(rate)
+  print cur + ': ' + str(rate * float(amount))
